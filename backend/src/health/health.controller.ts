ce9d1461a2fe11643f095d869b139deb07d1294b
phall168k <phall168k@gmail.com>
2026-09-07 12:49:33 +0700

    feat: add Get health check endpoint

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -1,15 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthResponseDto } from './dto/health-response.dto';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Health')
-@Controller({
-    path: 'health',
-})
+@Controller('health')
 export class HealthController {
     constructor(private readonly healthService: HealthService) {}
 
+    @Get()
     @ApiOperation({ summary: 'Healthy check' })
     @ApiOkResponse({ type: HealthResponseDto })
     public healthCheck(): HealthResponseDto {
