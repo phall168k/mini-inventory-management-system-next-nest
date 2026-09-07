1f420996b570dd6b2c67ba6bea87647c5f684847
phall168k <phall168k@gmail.com>
2026-09-07 09:16:54 +0700

    feat: configure global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,16 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`${process.env.API_PREFIX}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
