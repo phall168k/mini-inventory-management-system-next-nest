c1d4948200a6fcb9331d7b86ee28314bd783d542
phall168k <phall168k@gmail.com>
2026-09-07 09:15:37 +0700

    feat: set global api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix(`${process.env.API_PREFIX}`);
+  await app.listen(process.env.API_PORT ?? 8000);
 }
 bootstrap();
